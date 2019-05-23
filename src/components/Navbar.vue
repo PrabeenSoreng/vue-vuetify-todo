4c4b167d231dd9785c1c829766a49ea935542ece
Prabeen <prabeensoreng2119@gmail.com>
2019-05-23 18:49:36 +0530

    added popups and menus

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,11 +7,24 @@
         <span>Ninja</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
+      <!-- Dropdown menu -->
+      <v-menu offset-y>
+        <v-btn flat slot="activator" color="grey">
+          <v-icon left>expand_more</v-icon>
+          <span>Menu</span>
+        </v-btn>
+        <v-list>
+          <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-title>{{ link.text }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
       <v-btn flat color="grey">
         <span>Sign out</span>
         <v-icon right>exit_to_app</v-icon>
       </v-btn>
     </v-toolbar>
+    <!-- Navigation drawer -->
     <v-navigation-drawer v-model="drawer" app class="primary">
       <v-layout column align-center>
         <v-flex class="mt-5">
@@ -19,6 +32,9 @@
             <img src="/avatar-1.png">
           </v-avatar>
           <p class="white--text subheading mt-1">The Net Ninja</p>
+        </v-flex>
+        <v-flex class="mt-4 md-3">
+          <Popup/>
         </v-flex>
       </v-layout>
       <v-list>
@@ -36,7 +52,12 @@
 </template>
 
 <script>
+import Popup from "./Popup";
+
 export default {
+  components: {
+    Popup
+  },
   data() {
     return {
       drawer: false,
